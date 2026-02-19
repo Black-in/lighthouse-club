2f391680240bf23cc8f741551e40a44aa702fef0
Ayush Srivastava <ayush.srivastav@icloud.com>
2026-02-19 16:35:00 +0530

    Platform: Configure apps web src

---
+++ apps/web/src/store/user/useReviewModalStore.ts
@@ -0,0 +1,20 @@
+/*
+ * Lighthouse
+ * © 2026 ayushshrivastv
+ */
+
+import { create } from 'zustand';
+
+interface ReviewModalData {
+    open: boolean;
+    contractId: string | null;
+    show: (contractId: string) => void;
+    hide: () => void;
+}
+
+export const useReviewModalStore = create<ReviewModalData>((set) => ({
+    open: false,
+    contractId: null,
+    show: (contractId) => set({ open: true, contractId }),
+    hide: () => set({ open: false, contractId: null }),
+}));
---
+++ apps/web/src/types/next-auth.d.ts
@@ -0,0 +1,18 @@
+/*
+ * Lighthouse
+ * © 2026 ayushshrivastv
+ */
+
+import { UserType } from '@/app/api/auth/[...nextauth]/options';
+
+declare module 'next-auth' {
+    interface Session {
+        user: UserType;
+    }
+}
+
+declare module 'next-auth/jwt' {
+    interface JWT {
+        user: UserType;
+    }
+}
