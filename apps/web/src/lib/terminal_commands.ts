016c3d282c05a03a60e42da053346242e5c0aeeb
Ayush Srivastava <ayush.srivastav@icloud.com>
2026-02-19 14:18:00 +0530

    Quality: Establish apps web src

---
+++ apps/web/src/lib/terminal_commands.ts
@@ -0,0 +1,68 @@
+/*
+ * Lighthouse
+ * © 2026 ayushshrivastv
+ */
+
+const helpResponse = `
+WINTER COMMANDS:
+clear              Clear the terminal
+--help             Show available commands
+--commands         Show lighthouse commands
+--platform         Show platform details
+--hotkeys          Show hot keys/ shortcuts
+`;
+
+const hotKeysResponse = `
+HOT KEYS:
+Ctrl + Shift + ~           Switch Terminal Tabs
+Ctrl + Shift + d           Toggle shell
+`;
+
+const platformResponse = `
+PLATFORM DETAILS:
+portal              lighthouse
+version             1.0.0
+shell               winter
+`;
+
+const commandsResponse = `
+SHELL COMMANDS:
+winter build                to build the contract
+winter test                 to run the test file
+
+
+PREMIUM(+) SHELL COMMANDS:
+winter deploy --devnet      to deploy the contract on devnet
+winter deploy --mainnet     to deploy the contract on mainnet
+`;
+
+// instead of using winter deploy cmds like this use them like this
+// winter deploy --network devnet
+// winter deploy --network mainnet
+// winter deploy --network <custom-network>
+
+// const lighthouseBuildResponse = ``;
+
+export enum COMMAND_WRITER {
+    CLEAR = 'clear',
+    HELP = '--help',
+    HOT_KEYS = '--hotkeys',
+    PLATFORM = '--platform',
+    COMMANDS = '--commands',
+    lighthouse_BUILD = 'winter build',
+    lighthouse_TEST = 'winter test',
+    lighthouse_DEPLOY_DEVNET = 'winter deploy --devnet',
+    lighthouse_DEPLOY_MAINNET = 'winter deploy --mainnet',
+}
+
+export const CommandResponse: Record<COMMAND_WRITER, string> = {
+    [COMMAND_WRITER.CLEAR]: '',
+    [COMMAND_WRITER.HELP]: helpResponse,
+    [COMMAND_WRITER.HOT_KEYS]: hotKeysResponse,
+    [COMMAND_WRITER.PLATFORM]: platformResponse,
+    [COMMAND_WRITER.COMMANDS]: commandsResponse,
+    [COMMAND_WRITER.lighthouse_BUILD]: `this feature is forging in the lighthouse labs, arriving in an upcoming release...`,
+    [COMMAND_WRITER.lighthouse_TEST]: `this feature is forging in the lighthouse labs, arriving in an upcoming release...`,
+    [COMMAND_WRITER.lighthouse_DEPLOY_DEVNET]: `this feature is forging in the lighthouse labs, arriving in an upcoming release...`,
+    [COMMAND_WRITER.lighthouse_DEPLOY_MAINNET]: `this feature is forging in the lighthouse labs, arriving in an upcoming release...`,
+};
